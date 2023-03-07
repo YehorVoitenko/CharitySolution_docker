820c68add27cc10a14c4184faf24700e80503fa4
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-07 17:59:51 +0200

    Added project

--- CharitySolution_docker/CharitySolutionAPI/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from CharitySolutionAPI.models import OrganisationPost, Organisation, User
-
-admin.site.register(OrganisationPost)
-admin.site.register(Organisation)
-admin.site.register(User)
